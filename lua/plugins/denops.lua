42bfed59d833672870d20188ca4edaa1f088f6d0
KaniyaSimeji <dev@kanium.me>
2023-08-02 07:03:49 +0900

    Add: denops and skk

---
+++ lua/plugins/denops.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        'vim-denops/denops.vim',
+        lazy = false
+    },
+    {
+        "vim-skk/skkeleton",
+        dependencies = {
+            "vim-denops/denops.vim"
+        }
+    }
+}
