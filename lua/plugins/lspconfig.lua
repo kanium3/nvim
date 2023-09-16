49f293879cb9762718477148982d294b9742bccc
KaniyaSimeji <dev@kanium.me>
2023-09-16 20:29:06 +0900

    Update NeoVim Lua Setting

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -24,7 +24,35 @@ return {
                         on_attach = on_attach
                     }
                 end,
-                ["rust_analyzer"] = function() end
+                ["rust_analyzer"] = function() end,
+                ["lua_ls"] = function()
+                    require("lspconfig").lua_ls.setup {
+                        capabilities = capabilities,
+                        on_attach = on_attach,
+                        -- From https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
+                        on_init = function(client)
+                            local path = client.workspace_folders[1].name
+                            if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+                                client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+                                    Lua = {
+                                        runtime = {
+                                            version = 'LuaJIT'
+                                        },
+                                        workspace = {
+                                            checkThirdParty = false,
+                                            library = {
+                                                vim.env.VIMRUNTIME
+                                            }
+                                        }
+                                    }
+                                })
+
+                                client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+                            end
+                            return true
+                        end
+                    }
+                end
 
             }
             vim.api.nvim_create_autocmd("LspAttach", {
