c9ffa49d587767a9f1a2bbd57c591f3967b77845
KaniyaSimeji <dev@kanium.me>
2023-10-08 17:29:11 +0900

    Update: New settings

---
+++ lua/plugins/lsp_lens.lua
@@ -0,0 +1,5 @@
+return {
+    'VidocqH/lsp-lens.nvim',
+    event = { "LspAttach" },
+    opts = {}
+}
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,111 +1,97 @@
 return {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        dependencies = {
-            "williamboman/mason.nvim",
-            "aznhe21/actions-preview.nvim",
-            "SmiteshP/nvim-navic",
-            { "creativenull/efmls-configs-nvim", version = "v1.x.x" },
-        },
-        config = function()
-            require("mason").setup()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local mason_lspconfig = require("mason-lspconfig")
-            local on_attach = function(client, bufnr)
-                if client.server_capabilities["documentSymbolProvider"] then
-                    require("nvim-navic").attach(client, bufnr)
-                end
-            end
-            mason_lspconfig.setup_handlers({
-                function(server_name)
-                    require("lspconfig")[server_name].setup({
-                        capabilities = capabilities,
-                        on_attach = on_attach,
-                    })
-                end,
-                ["rust_analyzer"] = function() end,
-                ["lua_ls"] = function()
-                    require("lspconfig").lua_ls.setup({
-                        capabilities = capabilities,
-                        on_attach = on_attach,
-                        -- From https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
-                        on_init = function(client)
-                            local path = client.workspace_folders[1].name
-                            if
-                                not vim.loop.fs_stat(path .. "/.luarc.json")
-                                and not vim.loop.fs_stat(path .. "/.luarc.jsonc")
-                            then
-                                client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
-                                    Lua = {
-                                        runtime = {
-                                            version = "LuaJIT",
-                                        },
-                                        workspace = {
-                                            checkThirdParty = false,
-                                            library = {
-                                                vim.env.VIMRUNTIME,
-                                            },
-                                        },
-                                    },
-                                })
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			dependencies = {
+				"williamboman/mason.nvim",
+				"aznhe21/actions-preview.nvim",
+				"SmiteshP/nvim-navic",
+				{ "creativenull/efmls-configs-nvim", version = "v1.x.x" },
+				{ "folke/neodev.nvim", opts = {} },
+			},
+			config = function()
+				require("mason").setup()
+				require("neodev").setup({})
+				local capabilities = require("cmp_nvim_lsp").default_capabilities()
+				local mason_lspconfig = require("mason-lspconfig")
+				local on_attach = function(client, bufnr)
+					if client.server_capabilities["documentSymbolProvider"] then
+						require("nvim-navic").attach(client, bufnr)
+					end
+				end
+				mason_lspconfig.setup_handlers({
+					function(server_name)
+						require("lspconfig")[server_name].setup({
+							capabilities = capabilities,
+							on_attach = on_attach,
+						})
+					end,
+					["rust_analyzer"] = function() end,
+					["lua_ls"] = function()
+						require("lspconfig").lua_ls.setup({
+							capabilities = capabilities,
+							on_attach = on_attach,
+							settings = {
+								Lua = {
+									completion = {
+										callSnippet = "Replace",
+									},
+								},
+							},
+						})
+					end,
+					["tsserver"] = function()
+						require("lspconfig").tsserver.setup({
+							capabilities = capabilities,
+							on_attach = on_attach,
+							root_dir = require("lspconfig").util.root_pattern("package.json"),
+							single_file_support = false,
+						})
+					end,
+					["efm"] = function()
+						local languages = require("config").languages
+						local efmls_config = {
+							filetypes = vim.tbl_keys(languages),
+							settings = {
+								rootMarkers = { ".git/" },
+								languages = languages,
+							},
+							init_options = {
+								documentFormatting = true,
+								documentRangeFormatting = true,
+							},
+						}
 
-                                client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-                            end
-                            return true
-                        end,
-                    })
-                end,
-                ["tsserver"] = function()
-                    require("lspconfig").tsserver.setup({
-                        capabilities = capabilities,
-                        on_attach = on_attach,
-                        root_dir = require("lspconfig").util.root_pattern("package.json"),
-                        single_file_support = false,
-                    })
-                end,
-                ["efm"] = function()
-                    local languages = require("config").languages
-                    local efmls_config = {
-                        filetypes = vim.tbl_keys(languages),
-                        settings = {
-                            rootMarkers = { ".git/" },
-                            languages = languages,
-                        },
-                        init_options = {
-                            documentFormatting = true,
-                            documentRangeFormatting = true,
-                        },
-                    }
-
-                    require("lspconfig").efm.setup(vim.tbl_extend("force", efmls_config, {
-                        on_attach = on_attach,
-                        capabilities = capabilities,
-                    }))
-                end,
-            })
-            require("lspconfig").denols.setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-                root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc"),
-            })
-            vim.api.nvim_create_autocmd("LspAttach", {
-                callback = function(_)
-                    vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
-                    vim.keymap.set("n", "ff", "<cmd>lua vim.lsp.buf.format()<CR>")
-                    vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-                    vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-                    vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
-                    vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-                    vim.keymap.set("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
-                    vim.keymap.set("n", "rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
-                    vim.keymap.set({ "v", "n" }, "ca", require("actions-preview").code_actions)
-                    vim.keymap.set("n", "ge", "<cmd>lua vim.diagnostic.open_float()<CR>")
-                    vim.keymap.set("n", "g]", "<cmd>lua vim.diagnostic.goto_next()<CR>")
-                    vim.keymap.set("n", "g[", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
-                end,
-            })
-        end,
-    },
+						require("lspconfig").efm.setup(vim.tbl_extend("force", efmls_config, {
+							on_attach = on_attach,
+							capabilities = capabilities,
+						}))
+					end,
+				})
+				require("lspconfig").denols.setup({
+					capabilities = capabilities,
+					on_attach = on_attach,
+					root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc"),
+				})
+				vim.api.nvim_create_autocmd("LspAttach", {
+					callback = function(_)
+						vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+						vim.keymap.set("n", "ff", "<cmd>lua vim.lsp.buf.format()<CR>")
+						vim.keymap.set("n", "gr", "<cmd>Glance references<CR>")
+						vim.keymap.set("n", "gd", "<cmd>Glance definitions<CR>")
+						vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+						vim.keymap.set("n", "gi", "<cmd>Glance implementations<CR>")
+						vim.keymap.set("n", "gt", "<cmd>Glance type_definitions<CR>")
+						vim.keymap.set("n", "rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+						vim.keymap.set({ "v", "n" }, "ca", require("actions-preview").code_actions)
+						vim.keymap.set("n", "ge", "<cmd>lua vim.diagnostic.open_float()<CR>")
+						vim.keymap.set("n", "g]", "<cmd>lua vim.diagnostic.goto_next()<CR>")
+						vim.keymap.set("n", "g[", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
+					end,
+				})
+			end,
+		},
+	},
 }
