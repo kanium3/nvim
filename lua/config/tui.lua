9c537e4f13917dce3560848ee9ada9a95ae25cea
KaniyaSimeji <dev@kanium.me>
2023-10-31 23:08:51 +0900

    Update
    cant execude Mail and Music command

--- lua/config/tui.lua
+++ lua/config/tui.lua
@@ -1,22 +1,24 @@
-local mail = require("terminal").terminal:new({
-    layout = { open_cmd = "float" },
-    cmd = { "neomutt" },
-    autoclose = true,
-})
+if not jit.os == "Windows" then
+    local mail = require("terminal").terminal:new({
+        layout = { open_cmd = "float" },
+        cmd = { "neomutt" },
+        autoclose = true,
+    })
 
-vim.api.nvim_create_user_command("Mail", function()
-    mail:toggle(nil, true)
-end, {})
+    vim.api.nvim_create_user_command("Mail", function()
+        mail:toggle(nil, true)
+    end, {})
 
-local spotify = require("terminal").terminal:new({
-    layout = { open_cmd = "float" },
-    cmd = { "spt" },
-    autoclose = true,
-})
+    local spotify = require("terminal").terminal:new({
+        layout = { open_cmd = "float" },
+        cmd = { "spt" },
+        autoclose = true,
+    })
 
-vim.api.nvim_create_user_command("Music", function()
-    spotify:toggle(nil, true)
-end, {})
+    vim.api.nvim_create_user_command("Music", function()
+        spotify:toggle(nil, true)
+    end, {})
+end
 
 vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter", "TermOpen" }, {
     callback = function(args)
