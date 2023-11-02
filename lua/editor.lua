95764eea1f3e03d6d009ddd292c234c96bbb5c38
KaniyaSimeji <dev@kanium.me>
2023-11-03 08:01:10 +0900

    Update fileformat

--- lua/editor.lua
+++ lua/editor.lua
@@ -14,6 +14,7 @@ vim.g.markdown_fenced_languages = {
 vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
 vim.opt.listchars:append("eol:↴")
+vim.opt.ff = "unix"
 
 if vim.g.neovide then
     vim.o.guifont = "JetBrainsMono Nerd Font Mono,UD デジタル 教科書体 NP-B,IBM Plex Sans JP:h13"
