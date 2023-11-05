949e01b7084674a71dc54c523e8f64df45458944
KaniyaSimeji <dev@kanium.me>
2023-11-05 11:29:27 +0900

    Update
    Add symbol_outline.nvim

---
+++ lua/plugins/symbol_outline.lua
@@ -0,0 +1,13 @@
+return {
+    "simrat39/symbols-outline.nvim",
+    event = { "LspAttach" },
+    keys = {
+        {
+            "<leader>sb",
+            "<cmd>SymbolsOutline<CR>",
+            mode = { "n" },
+            desc = "Toggle Spectre",
+        }
+    },
+    opts = {}
+}
