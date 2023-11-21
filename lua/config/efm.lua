5bdf97f092924018a487082f74a8effcd2fc4296
KaniyaSimeji <dev@kanium.me>
2023-11-21 18:19:00 +0900

    Update
    change biome

--- lua/config/efm.lua
+++ lua/config/efm.lua
@@ -3,13 +3,13 @@ local stylua = require("efmls-configs.formatters.stylua")
 local selene = require("efmls-configs.linters.selene")
 local languagetool = require("efmls-configs.linters.languagetool")
 local eslint = require('efmls-configs.linters.eslint')
-local prettier = require('efmls-configs.formatters.prettier')
+local biome = require('efmls-configs.formatters.biome')
 local markdownlint = require('efmls-configs.linters.markdownlint')
 local textlint = require('efmls-configs.linters.textlint')
 
 M.languages = {
-    javascript = { eslint,prettier },
-    typescript = { eslint,prettier },
+    javascript = { eslint,biome },
+    typescript = { eslint,biome },
     lua = { stylua, selene },
     markdown = { languagetool,markdownlint,textlint },
 }
