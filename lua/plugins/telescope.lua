dbbf12ebf666d09f1dbf8c7af32cde3c077d77cc
KaniyaSimeji <dev@kanium.me>
2023-12-25 17:11:48 +0900

    Add telescope.nvim

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,18 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+        },
+    },
+    cmd = { "Telescope" },
+    config = function()
+        local telescope = require("telescope")
+        telescope.setup({})
+
+        telescope.load_extension("fzf")
+    end,
+}
