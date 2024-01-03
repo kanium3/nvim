0183d28c12b6dc95845133814fe573edf8e696c6
KaniyaSimeji <dev@kanium.me>
2024-01-03 17:59:52 +0900

    zoxide settings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,18 +7,42 @@ return {
             "nvim-telescope/telescope-fzf-native.nvim",
             build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
         },
+        "jvgrootveld/telescope-zoxide",
     },
     cmd = { "Telescope" },
     config = function()
         local telescope = require("telescope")
+        local z_utils = require("telescope._extensions.zoxide.utils")
         telescope.setup({
             extensions = {
                 package_info = {
                     theme = "ivy",
                 },
+                -- from https://github.com/jvgrootveld/telescope-zoxide
+                zoxide = {
+                    prompt_title = "zoxide",
+                    mappings = {
+                        default = {
+                            after_action = function(selection)
+                                print("Update to (" .. selection.z_score .. ") " .. selection.path)
+                            end,
+                        },
+                        ["<C-s>"] = {
+                            before_action = function(_)
+                                print("before C-s")
+                            end,
+                            action = function(selection)
+                                vim.cmd.edit(selection.path)
+                            end,
+                        },
+                        -- Opens the selected entry in a new split
+                        ["<C-q>"] = { action = z_utils.create_basic_command("split") },
+                    },
+                },
             },
         })
 
         telescope.load_extension("fzf")
+        telescope.load_extension('zoxide')
     end,
 }
