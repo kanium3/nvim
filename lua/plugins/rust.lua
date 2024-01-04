35e25e4cf52d996913a2907a1e38211a7e327f41
KaniyaSimeji <dev@kanium.me>
2024-01-04 19:47:38 +0900

    Exchange rust-tools to rustaceanvim
    Unsapported rust-tools.nvim

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,30 +1,30 @@
 return {
-    "simrat39/rust-tools.nvim",
-    ft = { "rust" },
-    event = { "LspAttach" },
-    dependencies = {
-        "SmiteshP/nvim-navic",
-    },
-    config = function()
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-        local on_attach = function(client, bufnr)
-            if client.server_capabilities["documentSymbolProvider"] then
-                require("nvim-navic").attach(client, bufnr)
+    {
+        "mrcjkb/rustaceanvim",
+        version = "^3",
+        ft = { "rust" },
+        event = { "LspAttach" },
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local on_attach = function(client, bufnr)
+                if client.server_capabilities["documentSymbolProvider"] then
+                    require("nvim-navic").attach(client, bufnr)
+                end
             end
-        end
-        require("rust-tools").setup({
-            server = {
-                capabilities = capabilities,
-                on_attach = on_attach,
-                settings = {
-                    ["rust-analyzer"] = {
-                        -- enable clippy on save
-                        checkOnSave = {
-                            command = "clippy",
+            vim.g.rustaceanvim = {
+                server = {
+                    capabilities = capabilities,
+                    on_attach = on_attach,
+                    settings = {
+                        ["rust-analyzer"] = {
+                            -- enable clippy when saving
+                            checkOnSave = {
+                                command = "clippy",
+                            },
                         },
                     },
                 },
-            },
-        })
-    end,
+            }
+        end,
+    },
 }
