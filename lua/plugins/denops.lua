82e5037f68093435c882e7ff6731f014b4ec1ed0
KaniyaSimeji <dev@kanium.me>
2024-02-22 16:56:04 +0900

    Update skk settings

--- lua/plugins/denops.lua
+++ lua/plugins/denops.lua
@@ -1,13 +1,13 @@
 -- WARNING: plugins that depend on denops.vim should not be lazy loaded with lazy.nvim configuration
 return {
     {
-        'vim-denops/denops.vim',
-        event = "VeryLazy"
+        "vim-denops/denops.vim",
+        event = "VeryLazy",
     },
     {
         "vim-skk/skkeleton",
         dependencies = {
-            "vim-denops/denops.vim"
+            "vim-denops/denops.vim",
         },
         keys = {
             { "<C-j>", "<Plug>(skkeleton-toggle)", mode = { "i", "c", "l" } },
@@ -18,13 +18,13 @@ return {
             -- Lazy loaded by using this function
             vim.fn["denops#plugin#load"]("skkeleton", this_path)
 
-            if not jit.os == 'Windows' then
-                vim.fn["skkeleton#config"] {
-                    useSkkServer = true
-                }
-            else
+            if jit.os == "Linux" then
+                vim.fn["skkeleton#config"]({
+                    sources = { "skk_server" },
+                })
+            elseif jit.os == "Windows" then
                 local windows_home = vim.fn.expand("~") -- ex:C://home/annko
-                vim.fn["skkeleton#config"] {
+                vim.fn["skkeleton#config"]({
                     globalDictionaries = {
                         windows_home .. "/skk/SKK-JISYO.L/SKK-JISYO.L",
                         windows_home .. "/skk/SKK-JISYO.geo/SKK-JISYO.geo",
@@ -39,39 +39,39 @@ return {
                         windows_home .. "/skk/SKK-JISYO.th.lisp-r12/SKK-JISYO.th.lisp-r12/SKK-JISYO.th-product.lisp",
                         windows_home .. "/skk/SKK-JISYO.th.lisp-r12/SKK-JISYO.th.lisp-r12/SKK-JISYO.th-character.lisp",
                         windows_home .. "/skk/SKK-JISYO.th.lisp-r12/SKK-JISYO.th.lisp-r12/SKK-JISYO.th-spellcard.lisp",
-                    }
-                }
+                    },
+                })
             end
-        end
+        end,
     },
     {
         "lambdalisue/kensaku.vim",
         dependencies = {
-            "vim-denops/denops.vim"
+            "vim-denops/denops.vim",
         },
         lazy = true,
         config = function()
             local plugins = require("config.plugin").get_plugin_name_path()
             local this_path = plugins["kensaku.vim"] .. "/denops/kensaku/main.ts"
             vim.fn["denops#plugin#load"]("kensaku", this_path)
-        end
+        end,
     },
     {
         "yuki-yano/fuzzy-motion.vim",
         dependencies = {
             "vim-denops/denops.vim",
-            "lambdalisue/kensaku.vim"
+            "lambdalisue/kensaku.vim",
         },
         keys = {
-            { "<leader>fz", "<Cmd>FuzzyMotion<CR>", desc = "fuzzy finder" }
+            { "<leader>fz", "<Cmd>FuzzyMotion<CR>", desc = "fuzzy finder" },
         },
-        init = function ()
+        init = function()
             vim.g.fuzzy_motion_matchers = { "fzf", "kensaku" }
         end,
         config = function()
             local plugins = require("config.plugin").get_plugin_name_path()
             local this_path = plugins["fuzzy-motion.vim"] .. "/denops/fuzzy-motion/main.ts"
             vim.fn["denops#plugin#load"]("fuzzy-motion", this_path)
-        end
+        end,
     },
 }
